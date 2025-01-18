5e8b761f7104bc2ae9cb791fd20f8b72049427c5
Aimejrh <ameerimzanbarakova@gmail.com>
2025-01-18 16:25:47 +0600

    problem

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+  ],
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  define: {
+    'process.env': {},
+  },
+});
